b78cb62e725f501be833ccfaf7a1f57a4f194308
Bernardo Narvaez <85888299+BernardN38@users.noreply.github.com>
2021-09-07 13:34:43 -0700

    Add files via upload

--- outer_space.sql
+++ outer_space.sql
@@ -7,22 +7,57 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
-CREATE TABLE planets
-(
+CREATE TABLE galaxies(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE stars (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  galaxy_id INTEGER NOT NULL REFERENCES galaxies
+  );
+
+CREATE TABLE planets(
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  orbits_around INTEGER REFERENCES stars
 );
 
+CREATE TABLE moons(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  planet_id INTEGER NOT NULL REFERENCES planets
+);
+
+
+
+INSERT INTO galaxies(name)
+VALUES 
+('milky way'),
+('andromeda');
+
+INSERT INTO stars (name, galaxy_id)
+VALUES 
+('sol', 1),
+('proxima centari',1),
+('gliese 876',1);
+
+
 INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+  (name, orbits_around, orbital_period_in_years)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('Earth', 1,1),
+  ('Mars', 1, 1.5),
+  ('Venus', 1,1.7),
+  ('Neptune', 1,1),
+  ('jupitar', 1,1),
+  ('Proxima Centauri b', 2,1),
+  ('Gliese 876 b', 3,1);
+
+INSERT INTO moons (name, planet_id)
+VALUES 
+('moon', 1),
+('titan', 4),
+('phobos', 2);
